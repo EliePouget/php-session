eaf97924a62b97d0524ff8383870ee72621af141
Elie Pouget <elie.pouget@etudiant.univ-reims.fr>
2022-09-06 10:54:24 +0200

    Mise en place démarage robuste de session

---
+++ src/Service/Session.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+class Session
+{
+    public static function start()
+    {
+        if (session_status() == PHP_SESSION_NONE) {
+            if (headers_sent() == true) {
+                throw new \Service\Exception\SessionException('Entête non modifiable');
+            }
+            session_start();
+        } elseif (session_status() == PHP_SESSION_DISABLED) {
+            throw new \Service\Exception\SessionException('Session désactiver');
+        } elseif (session_status() != PHP_SESSION_ACTIVE) {
+            throw new \Service\Exception\SessionException('Erreur inconnu');
+        }
+    }
+}
